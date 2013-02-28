2b7aeee8454d7b99a5d6461776dafbbc8bd70ccb
zogs <guichardsim@hotmail.com>
2013-02-28 23:31:48 +0100

    Zend Loader correction

--- controller/commentsController.php
+++ controller/commentsController.php
@@ -385,9 +385,9 @@
 							$video_id                      = getYTid($url);
 							$type                          = 'video';
 
-							require_once 'Zend/loader.php';
+							require_once 'Zend/Loader.php';
 							
-							Zend_Loader::loadClass('Zend_Gdata_Youtube');
+							Zend_Loader::loadClass('Zend_Gdata_YouTube');
 							
 							$yt                            = new Zend_Gdata_Youtube();
 							$yt->setMajorProtocolVersion(2);
--- webroot/phpinfo.php
+++ webroot/phpinfo.php
@@ -1,4 +1,4 @@
 <?php 
 
-echo $_SERVER['HTTP_HOST'];
+phpinfo();
 ?>
