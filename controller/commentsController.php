311a622f8ce68d475c230821785d0d9f28b8efe4
zogs <guichardsim@hotmail.com>
2013-03-07 20:02:21 +0100

    ReZendLoader

--- controller/commentsController.php
+++ controller/commentsController.php
@@ -367,7 +367,7 @@
 
 							require_once 'Zend/Loader.php';
 							
-							Zend_Loader::loadClass('Zend_Gdata_Youtube');
+							Zend_Loader::loadClass('Zend_Gdata_YouTube');
 							
 							$yt                            = new Zend_Gdata_YouTube();
 							$yt->setMajorProtocolVersion(2);
