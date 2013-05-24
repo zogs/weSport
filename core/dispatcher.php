4c67c9dbe847856af9b5668c1eef342741b1e748
zogs <guichardsim@hotmail.com>
2013-05-24 16:36:31 +0200

    debug cron

--- core/dispatcher.php
+++ core/dispatcher.php
@@ -42,7 +42,7 @@ class Dispatcher{
 
 		//autoload du controller
 		$controller =  new $name($this->request); //retourne une instance du bon controleur ( representé par le $name ! )
-		debug($controller);
+		
 		return $controller;
 	}
 
