f65b3e45009e9a8122c5c7997a9e00eaa7b54ca9
zogs <guichardsim@hotmail.com>
2013-07-15 14:41:33 +0200

    add Google Analytics

--- view/layout/default.php
+++ view/layout/default.php
@@ -144,22 +144,18 @@ if(isset($this->OpenGraphObject)) $openGraph = $this->request('events',$this->Op
 
  	
  	/*===========================================================
- 		GOOGLE FONTS
+ 		GOOGLE ANALYTICS
  	============================================================*/
-       WebFontConfig = {
-         google: { families: [ 'Lobster'] },      
-         fontinactive: function(fontFamily, fontDescription) { /*alert('Font '+fontFamily+' is currently not available'); */}
-       };
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-42468932-1', 'zogs.org');
+	  ga('send', 'pageview');
 
-       (function() {
-         var wf = document.createElement('script');
-         wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-             '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-         wf.type = 'text/javascript';
-         wf.async = 'true';
-         var s = document.getElementsByTagName('script')[0];
-         s.parentNode.insertBefore(wf, s);
-       })();
+
+
+
 </script>
 
 
