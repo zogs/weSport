08348f23f64dd05708a2faaaa0fb95cc6335a584
Mr.zogs <guichardsim@gmail@gmail.com>
2014-01-20 12:03:23 +0100

    add ascii note

--- view/layout/default.php
+++ view/layout/default.php
@@ -6,6 +6,20 @@
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<!-- 
+    888       888                 .d8888b.                            888         .d888             
+    888   o   888                d88P  Y88b                           888        d88P"              
+    888  d8b  888                Y88b.                                888        888                
+    888 d888b 888  .d88b.         "Y888b.   88888b.   .d88b.  888d888 888888     888888 888d888 
+    888d88888b888 d8P  Y8b           "Y88b. 888 "88b d88""88b 888P"   888        888    888P"   
+    88888P Y88888 88888888 888888      "888 888  888 888  888 888     888        888    888         
+    8888P   Y8888 Y8b.           Y88b  d88P 888 d88P Y88..88P 888     Y88b.  d8b 888    888     
+    888P     Y888  "Y8888         "Y8888P"  88888P"   "Y88P"  888      "Y888 Y8P 888    888     
+                                            888                                                     
+                                            888                                                     
+                                            888                                                     
+
+-->
 <?php 
 
 //Si cette page est un objet OpenGraph on recupere les balises metas qui vont bien
